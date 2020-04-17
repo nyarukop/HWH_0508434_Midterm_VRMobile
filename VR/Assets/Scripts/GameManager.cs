ba299570c777e65e528f5a841779c3000a513926
nyarukop <62408785+nyarukop@users.noreply.github.com>
2020-04-17 11:17:07 +0800

    完成APK檔並發布

--- VR/Assets/Scripts/GameManager.cs
+++ VR/Assets/Scripts/GameManager.cs
@@ -2,8 +2,29 @@
 
 public class GameManager : MonoBehaviour
 {
-   
-    public Animator anibox;
-    public AudioClip boxOpen;
+    [Header("喇叭控制器")]
     public AudioSource aud;
+
+    [Header("箱的動畫控制器")]
+    public Animator anibox;
+
+    [Header("開箱音效")]
+    public AudioClip soundOpen;
+
+    private int countBox; //看到箱子的次數
+
+    public void LookBox()
+    {
+        countBox++;
+        if (countBox == 1)
+        {
+            aud.PlayOneShot(soundOpen, 5);
+        }
+        else if (countBox == 2)
+        {
+            aud.PlayOneShot(soundOpen, 4.5f);
+            anibox.SetTrigger("開箱觸發器");
+        }
+    }
 }
+
